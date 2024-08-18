4673b893ef70dbf8c53d3df529b9f9361381cd91
Pichau <igorof06@outlook.com>
2024-08-18 14:06:52 -0300

    first commit

---
+++ file-manager/src/test/java/com/igorbarreto/file_manager/FileManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.igorbarreto.file_manager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FileManagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
